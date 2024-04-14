308067db113e368e07c3f8c3c0d2e948f62bc15d
Andrej Lozanovski <andrejlozanovski@hotmail.com>
2024-04-14 11:46:26 +0200

    filtercategorybutton added with css

---
+++ starter/src/components/FilterCategoryButton.tsx
@@ -0,0 +1,13 @@
+import "./ComponentsStyle/filterButton.css";
+
+const FilterCategoryButton = () => {
+  return (
+    <div className="btns">
+      <button className="p-2 m-2 btn-cat">Најбарани категории</button>
+      <button className="p-2 m-2 btn-cat">Најбарани категории</button>
+      <button className="p-2 m-2 btn-cat">Најбарани категории</button>
+    </div>
+  );
+};
+
+export default FilterCategoryButton;
