896412b278910b24563e4e9e946b18680ab27bab
Maja Karalioska <m_karalioska@live.com>
2024-04-14 13:15:17 +0200

    geo popup finished

--- starter/src/app/layout.tsx
+++ starter/src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import GeoPopup from "@/components/GeoPopup";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
